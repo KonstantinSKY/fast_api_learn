6e89e296a6533915e17e850984558afc821a6bda
SKY <sky012877@gmail.com>
2024-01-04 13:22:14 -0800

    main.py - Added :

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Learning App"
+)
+
+
+@app.get("/")
+def get_hello():
+    return "Hello World!"
+
+
+f_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in f_users if user.get("id") == user_id]
+
+
+f_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+@app.get("/trades/")
+def get_trades(limit: int = 1, offset: int = 1):
+    return f_trades[offset:][:limit]
+
+fake_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users2))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
