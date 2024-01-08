e2be5348fa817389b87b8118c7272e91658ef225
SKY <sky012877@gmail.com>
2024-01-07 23:56:57 -0800

    migrations/env.py - Modified :

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,14 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from models.models import metadata
+
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from src.auth.models import metadata
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
