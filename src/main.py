1e33eb2f080a885b4712d4a38ec25d5d0104c01f
SKY <sky012877@gmail.com>
2024-01-10 11:54:10 -0800

    src/main.py - Modified :

--- src/main.py
+++ src/main.py
@@ -18,24 +18,6 @@ app = FastAPI(
 
 app.include_router(routers)
 
-# fastapi_users = FastAPIUsers[User, int](
-#     get_user_manager,
-#     [auth_backend],
-# )
-
-# app.include_router(
-#     fastapi_users.get_auth_router(auth_backend),
-#     prefix="/auth/jwt",
-#     tags=["auth"],
-# )
-# app.include_router(
-#     fastapi_users.get_register_router(UserRead, UserCreate),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
-#
-
-#
 #
 # # # Благодаря этой функции клиент видит ошибки, происходящие на сервере, вместо "Internal server error"
 # # @app.exception_handler(ResponseValidationError)
@@ -75,13 +57,13 @@ app.include_router(routers)
 #     degree: Optional[List[Degree]] = []
 #
 #
-# current_user = fastapi_users.current_user()
+current_user = fastapi_users.current_user()
 #
 #
-# @app.get("/protected-route")
-# def protected_route(user: User = Depends(current_user)):
-#     return f"Hello, {user.username}"
-#
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
 #
 # @app.get("/unprotected-route")
 # def unprotected_route():
