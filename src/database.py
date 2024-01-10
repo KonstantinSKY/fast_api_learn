3dde75a036dd0b35aa6d754fa2ae40afc4385762
SKY <sky012877@gmail.com>
2024-01-09 19:41:44 -0800

    src/database.py - Modified :

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 from typing import AsyncGenerator
-
+from importlib import import_module
+from setting import APPS
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -9,10 +10,11 @@ from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-# Base: DeclarativeMeta = declarative_base()
 class Base(DeclarativeBase):
     pass
 
+
+metadata = Base.metadata
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -21,3 +23,22 @@ async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+def alembic_config_set_sections(config):
+    section = config.config_ini_section
+    config.set_section_option(section, "DB_HOST", DB_HOST)
+    config.set_section_option(section, "DB_PORT", DB_PORT)
+    config.set_section_option(section, "DB_USER", DB_USER)
+    config.set_section_option(section, "DB_NAME", DB_NAME)
+    config.set_section_option(section, "DB_PASS", DB_PASS)
+
+
+def import_app_models():
+    for app in APPS:
+        models_module = f"{app}.models"
+        try:
+            import_module(models_module)
+
+        except ModuleNotFoundError:
+            print(f"App Model for models not found: {app}")
