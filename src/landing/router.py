463b007c714640fa17292d5f83f8a117dcf28fa2
SKY <sky012877@gmail.com>
2024-01-10 11:54:09 -0800

    src/landing/router.py - Modified :

--- src/landing/router.py
+++ src/landing/router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from auth.config import fastapi_users
+from auth.models import User
 
 router = APIRouter(
     tags=["LandingPage"]
@@ -8,3 +11,22 @@ router = APIRouter(
 @router.get("/", operation_id="getHello")
 def get_hello():
     return "Hello World!"
+
+
+current_user = fastapi_users.current_user()
+
+
+@router.get("/protected-route", tags=["ProtectedLanding"])
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@router.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, Anonym"
+
+#
+# @app.get("/users/{user_id}", response_model=List[User])
+# def get_user(user_id: int):
+#     return [user for user in f_users if user.get("id") == user_id]
+#
