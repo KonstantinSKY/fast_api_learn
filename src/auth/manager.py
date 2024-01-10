ef4e55c276ddcd31abfbd3bc51e8bd6101b58841
SKY <sky012877@gmail.com>
2024-01-10 11:03:41 -0800

    src/auth/manager.py - Modified :

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,4 +1,4 @@
-import uuid
+from typing import Optional
 from typing import Optional
 
 from fastapi import Depends, Request
@@ -6,15 +6,14 @@ from fastapi_users import BaseUserManager, IntegerIDMixin
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .models import User
+from config import SECRET_RESET_TOKEN, SECRET_VERIFICATION_TOKEN
 from database import get_async_session
+from .models import User
 
-SECRET = "SECRET"
 
-
-class UserManager(IntegerIDMixin, BaseUserManager[User, uuid.UUID]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET_RESET_TOKEN
+    verification_token_secret = SECRET_VERIFICATION_TOKEN
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
